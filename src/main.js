b6c28ea867db865f451c9c47d12985b2dbfae960
Shabin-k <helloshabink@gmail.com>
2023-11-09 16:11:49 +0530

    hide

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import { PuppeteerCrawler, log } from "crawlee";
 import { router } from "./routes.js";
 
-
 log.setLevel(log.LEVELS.DEBUG);
 
 log.debug("Setting up crawler.");
 const crawler = new PuppeteerCrawler({
   requestHandler: router,
   maxRequestsPerCrawl: 50,
+  //headless: false,
 });
 
 await crawler.run(["https://warehouse-theme-metal.myshopify.com/collections"]);
